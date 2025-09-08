25cd4d0ea7258924b06a01055c7d02f5f9270b5a
EFTHYMIOS PANTOUVAKIS <epadouvakis@gmail.com>
2025-09-08 14:18:01 +0300

    Temporarily disable no-explicit-any rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // ➕ Προσθέτουμε override για να απενεργοποιήσουμε το rule
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
