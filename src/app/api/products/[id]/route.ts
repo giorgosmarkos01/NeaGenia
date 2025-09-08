30386bc3ddf25fb4fd10a5d63283fd208c005ec9
EFTHYMIOS PANTOUVAKIS <epadouvakis@gmail.com>
2025-09-08 16:28:36 +0300

    deploy debugg

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,16 +1,21 @@
 import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+export async function GET(req: NextRequest) {
   try {
-    const [rows]: any = await db.query("SELECT * FROM item WHERE id = ?", [
-      params.id,
-    ]);
-    if (rows.length === 0)
+    const url = new URL(req.url);
+    const id = url.pathname.split("/").pop(); // παίρνει το [id] από το path
+
+    if (!id) {
+      return NextResponse.json({ error: "Missing ID" }, { status: 400 });
+    }
+
+    const [rows]: any = await db.query("SELECT * FROM item WHERE id = ?", [id]);
+
+    if (rows.length === 0) {
       return NextResponse.json({ error: "Not found" }, { status: 404 });
+    }
+
     return NextResponse.json(rows[0]);
   } catch (error) {
     console.error(error);
